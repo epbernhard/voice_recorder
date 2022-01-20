af2dd28c64c05e511be7b1ad0a444791831ab122
manu-admin <e.p.bernhard@sheffield.ac.uk>
2022-01-20 14:13:55 +0100

    voice recorder V1.0.0

--- v_rec.py
+++ v_rec.py
@@ -1,42 +1,63 @@
-import numpy as np
+"""A simple voice recorder"""
+import time
 import sounddevice
 from scipy.io.wavfile import write
 from tqdm import tqdm
-import time
 
-class recorder():
+class Recorder():
+    """Class Recorder containing methods for recording."""
 
-    def __init__(self, sec, fs, sample = []):
+    def __init__(self, sec, f_s, sample = []):
+        """Init the class Recorder.
+
+        Parameters:
+        sec (float): recording time in seconds.
+        f_s (int): numbers of frames.
+
+        Keywords:
+        sample (array): array of float containing the recording.
+        """
 
         self.sec = sec # Recording time in seconds
-        self.fs = fs # Sampling frequency
+        self.f_s = f_s # Sampling frequency
         self.sample = sample
 
     def record(self, channels = 2, pgbar = True):
+        """Method for the recording.
+
+        Keywords:
+        channels (int): the number of recording channels.
+        pgbar (bool): whether to display a progress bar while recording.
+        """
         print("Recording...")
-        self.sample = sounddevice.rec(int(self.sec * self.fs ), samplerate = self.fs , channels = channels)
-        if pgbar == True:
+        self.sample = sounddevice.rec(int(self.sec * self.f_s ), \
+                                      samplerate = self.f_s , \
+                                      channels = channels)
+        if pgbar:
             [time.sleep(1) for i in tqdm(range(0, self.sec))]
         else:
             sounddevice.wait()
         print("Finished recording.")
 
     def playback(self):
+        """Method for the playback."""
+
         if len(self.sample) == 0:
             raise ValueError("Sorry, there is nothing to play back.")
-        else:
-            sounddevice.play(self.sample, self.fs)
 
+        sounddevice.play(self.sample, self.f_s)
+        sounddevice.wait()
 
 if __name__ == "__main__":
-    time_s = 3
-    fs = 44100
 
-    voice_recorder = recorder(time_s, fs)
+    NAME = 'yes'
+    TIME_S = 3
+    F_S = 44100
+
+    voice_recorder = Recorder(TIME_S, F_S)
     voice_recorder.record()
-    
+
     # If you want to play back what you just said
     # voice_recorder.playback()
 
-    yes = voice_recorder.sample
-    write("yes.wav", fs , yes)
+    write(NAME+".wav", F_S , voice_recorder.sample)
