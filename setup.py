8b10dfc7848354630a39f48fd3574dfe0cbbbd97
manu-admin <e.p.bernhard@sheffield.ac.uk>
2022-01-20 15:04:32 +0100

    correcting the install file

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/epbernhard/voice_recorder",
-    install_requires=['numpy', 'time', 'sounddevice', 'scipy', 'tqdm'],
+    install_requires=['time', 'sounddevice', 'scipy', 'tqdm'],
     packages=[],
     classifiers=[
         "Programming Language :: Python :: 3",
