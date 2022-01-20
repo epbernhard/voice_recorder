4249040036647a01bf579b6ff77132a4d8507bc2
manu-admin <e.p.bernhard@sheffield.ac.uk>
2022-01-20 14:30:32 +0100

    adding installation files

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="voice_recorder",
+    version="1.0.0",
+    author="E.P.W. Bernhard",
+    author_email="manu.p.bernhard@gmail.com",
+    description="Just a simple voice recorder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/epbernhard/voice_recorder",
+    install_requires=['time', 'sounddevice', 'scipy', 'tqdm'],
+    packages=['voice_recorder'],
+    package_data={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
