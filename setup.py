337af4ee701ca612468449caab3c117001964d63
manu-admin <e.p.bernhard@sheffield.ac.uk>
2022-01-20 15:14:16 +0100

    correcting the install file

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/epbernhard/voice_recorder",
-    install_requires=['time', 'sounddevice', 'scipy', 'tqdm'],
+    install_requires=['time', 'sounddevice', 'tqdm'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
